0efe8e1f039991dc76f831441da51fed6db82288
mohiyasqom-design <mohiyasqom@gmail.com>
2025-11-16 14:24:18 +0330

    Update and rename LoadingSpinner.tsx to LoadingSpinner.js

--- components/LoadingSpinner.js
+++ components/LoadingSpinner.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
-interface LoadingSpinnerProps {
-  large?: boolean;
-}
-
-const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({ large = false }) => {
+const LoadingSpinner = ({ large = false }) => {
   const sizeClass = large ? 'w-8 h-8' : 'w-5 h-5';
   const borderClass = large ? 'border-4' : 'border-2';
 
